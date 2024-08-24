9067383c600a4b65ec8a18c63b91f5e2a4f5e5fb
Muslip Laptop bw015 <muszczlim@gmail.com>
2024-08-24 10:41:00 +0700

    Ganti tampilan sedikit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Testing Laravel
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://www.muslimaswaja.id">Website</a>
+                    <a href="https://instagram.com/muslimbinazizi">Instagram</a>
                 </div>
             </div>
         </div>
